b3b8bf097fce10d164b4776042364451a3c37305
Denis Kornienko <korney.turk@gmail.com>
2025-01-07 21:59:43 +0300

    add checking db is ready

--- scripts/prepare.sh
+++ scripts/prepare.sh
@@ -8,6 +8,14 @@ go mod tidy
 echo "[prepare.sh] Проверяем, что контейнер с БД запущен..."
 docker compose up -d
 
+
+echo "[prepare.sh] Ожидание готовности базы данных..."
+until docker compose exec db pg_isready -U validator -d project-sem-1; do
+  echo "База данных ещё не готова, ждём 5 секунд..."
+  sleep 5
+done
+echo "База данных готова."
+
 echo "[prepare.sh] Создаём таблицу prices (если не существует)..."
 docker compose exec db \
   psql -U validator -d project-sem-1 -c \
